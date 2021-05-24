6d0c4f9727f21ca102d0014658c4c2c3456e8576
zhangtongxi <834155292@qq.com>
2021-05-24 23:30:40 +0800

    com.zboy.example.structural.adapter.example05.Test

---
+++ Structural-Adapter/src/main/java/com/zboy/example/structural/adapter/example05/RoundHole.java
@@ -0,0 +1,19 @@
+package com.zboy.example.structural.adapter.example05;
+
+public class RoundHole {
+    private double radius;
+
+    public RoundHole(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public boolean fits(RoundPeg peg) {
+        boolean result;
+        result = (this.getRadius() >= peg.getRadius());
+        return result;
+    }
+}
---
+++ Structural-Adapter/src/main/java/com/zboy/example/structural/adapter/example05/SquarePegAdapter.java
@@ -0,0 +1,17 @@
+package com.zboy.example.structural.adapter.example05;
+
+public class SquarePegAdapter extends RoundPeg {
+    private SquarePeg peg;
+
+    public SquarePegAdapter(SquarePeg peg) {
+        this.peg = peg;
+    }
+
+    @Override
+    public double getRadius() {
+        double result;
+        // Calculate a minimum circle radius, which can fit this peg.
+        result = (Math.sqrt(Math.pow((peg.getWidth() / 2), 2) * 2));
+        return result;
+    }
+}
