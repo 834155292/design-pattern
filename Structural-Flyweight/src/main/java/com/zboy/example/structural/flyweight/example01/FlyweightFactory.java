75c816e0892647e9555e53080111bcd11bf897f5
zhangtongxi <834155292@qq.com>
2021-06-30 21:12:45 +0800

    完善享元模式的代码

--- Structural-Flyweight/src/main/java/com/zboy/example/structural/flyweight/example01/FlyweightFactory.java
+++ Structural-Flyweight/src/main/java/com/zboy/example/structural/flyweight/example01/FlyweightFactory.java
@@ -15,4 +15,19 @@ public class FlyweightFactory {
         }
         return flyweight;
     }
+
+    //转换成静态内部类可以防止被其他客户端实例化。客户对象永远不会引用该内部类。享元应该由享元工厂创建。
+    private static class ConcreteFlyweight implements Flyweight {
+        private String key;
+
+        ConcreteFlyweight(String key) {
+            this.key = key;
+            System.out.println("具体享元" + key + "被创建！");
+        }
+
+        public void operation(UnsharedConcreteFlyweight outState) {
+            System.out.print("具体享元" + key + "被调用，");
+            System.out.println("非享元信息是:" + outState.getInfo());
+        }
+    }
 }
--- Structural-Flyweight/src/main/java/com/zboy/example/structural/flyweight/example02/Shape.java
+++
@@ -1,5 +0,0 @@
-package com.zboy.example.structural.flyweight.example02;
-
-public interface Shape {
-    void draw();
-}
